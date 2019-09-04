85514446b835ab325ba70fc79338e2c18cdf037e
Irving <guerravargasirving@gmail.com>
2019-09-04 18:17:45 -0500

    Termino en java

--- programa.cpp
+++ programa.cpp
@@ -2,47 +2,118 @@
 #include <stdlib.h>	
 #include <iostream>
 #include <fstream>
+#include <string.h> 
+
 using namespace std;
+	  
+    string arrayEstados[99];
+    string arrayCuando0[99];
+    string arrayCuando1[99]; //|  ---
+    
+    bool esQ(int indice);
+    bool valueInArray(string val, string arr[]);
 
 int main () {
-	//Primero copiamos el el AFND al AFD	
+	//Primero copiamos el el AFND al AFD
 	FILE *file1 , *file2;
     int data1 = 0;
-    
+    int q = 0;
+
     file1 = fopen ( "AFND.txt", "r" ); //Abrimos en NO determinista
     file2 = fopen ( "AFD.txt" , "w" ); //Creamos el DETERMINISTA
     
     while ( (data1 = fgetc ( file1 )) != EOF ) {
         fputc ( data1, file2 );
+    	switch(data1){
+			case '-': //Siguiente q
+				q++;
+			break;
+			case '{':
+				//Empieza el primer estado que debemos de crear
+				data1 = fgetc ( file1 ); //contiene la letra
+				arrayEstados[q] = data1;
+			break;
+			case ',':
+				//Es un estado con mas de 1
+				data1 = fgetc ( file1 ); //contiene la letra
+				strcat(original,concat); 
+				arrayEstados[q] = arrayEstados[q] + "," + data1;
+			break;
+			case '}':
+				//Empezara a ver los estados
+				data1 = fgetc ( file1 ); //contiene | No nos interesa
+				data1 = fgetc ( file1 );  //Contiene { de el caso 0
+				while ( (data1 = fgetc ( file1 )) != '}' ) {
+				 	switch(data1){
+				 		case ',':
+							//Es un estado con mas de 1
+							data1 = fgetc ( file1 ); //contiene la letra
+							strcat(arrayCuando0[q], ",");
+							strcat(arrayCuando0[q], data1);
+						break;
+						default:
+							arrayCuando0[q] = data1;
+					}
+				}
+				data1 = fgetc ( file1 ); //contiene | No nos interesa
+				data1 = fgetc ( file1 );  //Contiene { de el caso 1
+				while ( (data1 = fgetc ( file1 )) != '}' ) {
+				 	switch(data1){
+				 		case ',':
+							//Es un estado con mas de 1
+							data1 = fgetc ( file1 ); //contiene la letra
+							strcat(arrayCuando1[q], ",");
+							strcat(arrayCuando1[q], data1);
+						break;
+						default:
+							arrayCuando1[q]= data1;
+					}
+				}
+			break;
+		}
     }
     
     fclose ( file1 ); //Cerramos el NO determinista
+    
+    for(int i=0 ; i < q ; i++){
+    	//Preguntamos si el primer caso en 0 ya esta considerado en los casos normales
+    	if(esQ(i)){
+    		
+		}
+	}
+   	
 	
 	//Ahora seguimos leyendo los casos del AFND para agregarlos al AFD
 	
 	file1 = fopen ( "AFND.txt", "r" ); //Abrimos de nuevo el NO determinista
 	
-	char letra;
 
-	int aux0or1 = 0; //auxiliar que nos dice si estara leyendo el 0 o 1
-	
-	while ( (letra = fgetc ( file1 )) != EOF ) {
-       switch(letra){
-			case '-': //Siguiente q
-			
-			break;
-			case '{':
-				//Empieza el primer estado que debemos de crear
-			break;
-			case '}':
-				
-			break;
-		}
-    }
-    
     fclose ( file1 );
     fclose ( file2 );
 	
  return 0;
+
+ 
 }
+
+
+ bool esQ(int indice){
+ 	
+ 	
+ }
+ 
+ int valueinarray(float val, float arr[])
+{
+    int i;
+    for(i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+    {
+        if(arr[i] == val)
+            return 1;
+    }
+    return 0;
+}
+ 
+ 
+ 
+
 
---
+++ soft/src/soft/Soft.java
@@ -0,0 +1,176 @@
+package soft;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author Irving-Guerra
+ */
+public class Soft {
+
+    static String[] arrayEstados = new String[99];
+    static String[] arrayCuando0 = new String[99];
+    static String[] arrayCuando1 = new String[99];
+    
+    static int q = 0;
+
+    static char letra;
+    
+    public static void main(String[] args) {
+        try {
+            FileInputStream fis = new FileInputStream("AFND.txt");
+            while (fis.available() > 0) {
+                    letra = (char) fis.read();
+                    switch(letra){
+                        case '-': //Siguiente q
+                                q++;
+                        break;
+                        case '{':
+                                //Empieza el primer estado que debemos de crear
+                                letra = (char) fis.read(); //contiene la letra
+                                arrayEstados[q] = String.valueOf(letra);
+                        break;
+                        case ',':
+                                //Es un estado con mas de 1
+                                letra = (char) fis.read(); //contiene la letra
+                                arrayEstados[q] = arrayEstados[q] + "," + letra;
+                        break;
+                        case '}':
+                                //Empezara a ver los estados
+                                letra = (char) fis.read(); //contiene la letra //contiene | No nos interesa
+                                letra = (char) fis.read(); //contiene la letra //Contiene { de el caso 0
+                                while ( (letra = (char) fis.read()) != '}' ) {
+                                        switch(letra){
+                                                case ',':
+                                                        //Es un estado con mas de 1
+                                                        letra = (char) fis.read(); //contiene la letra
+                                                        arrayCuando0[q] += "," + letra;
+                                                break;
+                                                default:
+                                                        arrayCuando0[q] = String.valueOf(letra);
+                                        }
+                                }
+                                letra = (char) fis.read();  //contiene | No nos interesa
+                                letra = (char) fis.read();   //Contiene { de el caso 1
+                                while ( (letra = (char) fis.read()) != '}' ) {
+                                        switch(letra){
+                                                case ',':
+                                                        //Es un estado con mas de 1
+                                                        letra = (char) fis.read(); //contiene la letra
+                                                        arrayCuando1[q] += "," + letra;
+                                                break;
+                                                default:
+                                                        arrayCuando1[q] = String.valueOf(letra);
+                                        }
+                                }
+                        break;
+                    }
+            }
+            fis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+                //Copiamos directamente el archivo
+        try {
+            FileInputStream fis = new FileInputStream("AFND.txt");
+            FileOutputStream fos = new FileOutputStream("AFD.txt");
+            while (fis.available() > 0) {
+                    letra = (char) fis.read();
+                    fos.write(letra);
+            }
+            fis.close();
+            //Verificaremos si los estados en 0 o 1 estan en nuestro original
+
+            //Convertimos el array en lista
+
+            List<String> list = Arrays.asList(arrayEstados);
+            
+            int aux = q;
+            
+            for(int i = 0 ; i < aux ; i++){
+                if(list.contains(arrayCuando0[i])){
+                    System.out.println("Lo tiene");
+                }else{
+                    if(!arrayCuando0[i].equals("0")){
+                        //Si no es 0 agrega el nuevo estado
+                        arrayEstados[q] = arrayCuando0[i];
+                        System.out.println(arrayEstados[q]);
+                        //y aparte añadimos sus 0 y 1
+                        String[] estados = arrayCuando0[i].split(",");
+                        arrayCuando0[q] = "";
+                        for(int j = 0 ; j<estados.length ; j++){
+                            int index = list.indexOf(estados[j]);
+                            if(!arrayCuando0[index].equals("0")){
+                                arrayCuando0[q] += arrayCuando0[index]+",";
+                            }
+                        }
+                        arrayCuando1[q] = "";
+                        for(int j = 0 ; j<estados.length ; j++){
+                            int index = list.indexOf(estados[j]);
+                            if(!arrayCuando1[index].equals("0")){
+                                arrayCuando1[q] += arrayCuando1[index]+",";
+                            }
+                        }
+                        arrayCuando0[q] = arrayCuando0[q].substring(0, arrayCuando0[q].length()-1); 
+                        arrayCuando1[q] = arrayCuando1[q].substring(0, arrayCuando1[q].length()-1); 
+                        aux++;
+                        System.out.println(arrayCuando0[q]);
+                        System.out.println(arrayCuando1[q]);
+                       
+                        String cadena = "{"+ arrayEstados[q]+"}|{"+arrayCuando0[q]+"}|{"+arrayCuando1[q]+"}-\n";
+                        fos.write(cadena.getBytes());
+                    }
+                }
+                if(list.contains(arrayCuando1[i])){
+                    System.out.println("Lo tiene");
+                }else{
+                    if(!arrayCuando1[i].equals("0")){
+                        //Si no es 0 agrega el nuevo estado
+                        arrayEstados[q] = arrayCuando1[i];
+                        System.out.println(arrayEstados[q]);
+                        //y aparte añadimos sus 0 y 1
+                        String[] estados = arrayCuando1[i].split(",");
+                        arrayCuando0[q] = "";
+                        for(int j = 0 ; j<estados.length ; j++){
+                            int index = list.indexOf(estados[j]);
+                            if(!arrayCuando0[index].equals("0")){
+                                arrayCuando0[q] += arrayCuando0[index]+",";
+                            }
+                        }
+                        arrayCuando1[q] = "";
+                        for(int j = 0 ; j<estados.length ; j++){
+                            int index = list.indexOf(estados[j]);
+                            if(!arrayCuando1[index].equals("0")){
+                                arrayCuando1[q] += arrayCuando1[index]+",";
+                            }
+                        }
+                        arrayCuando0[q] = arrayCuando0[q].substring(0, arrayCuando0[q].length()-1); 
+                        arrayCuando1[q] = arrayCuando1[q].substring(0, arrayCuando1[q].length()-1); 
+                        aux++;
+                        System.out.println(arrayCuando0[q]);
+                        System.out.println(arrayCuando1[q]);
+                       
+                        String cadena = "{"+ arrayEstados[q]+"}|{"+arrayCuando0[q]+"}|{"+arrayCuando1[q]+"}-\n";
+                        fos.write(cadena.getBytes());
+                    }
+                }
+            }
+
+            fos.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        
+            
+        
+        
+    }
+    
+}
